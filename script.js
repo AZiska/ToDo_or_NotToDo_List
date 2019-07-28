3b492e373e82aca1c29a67b6cf29afaa18f2a1e3
AZiska <antjebommel@Antjes-MacBook.fritz.box>
2019-07-28 15:30:53 +0200

    Tasks are now stored in an array.
    This makes future use easier (e.g. storing the tasks in a database).

--- script.js
+++ script.js
@@ -3,50 +3,58 @@ const notToDoButton = document.getElementById('notToDoButton');
 const deleteButton = document.getElementById('deleteButton');
 const inputField = document.getElementById('inputField');
 
+const tasks = {
+    toDo: ['Read a good book'],
+    notToDo: ['Read a boring book']
+};
 
 toDoButton.onclick = () => {
-    addTaskToList('toDoButton')
+    const newTask = inputField.value;
+    if (newTask !== '') {
+        tasks.toDo.push(newTask);
+        renderTasks(tasks.toDo);
+    }
 }
 
 notToDoButton.onclick = () => {
-    addTaskToList('notToDoButton')
+    const newTask = inputField.value;
+    if (newTask !== '') {
+        tasks.notToDo.push(newTask);
+        renderTasks(tasks.notToDo);
+    }
+}
+
+const renderTasks = (taskArray) => {
+    let htmlList;
+    let startIndex;
+    if (taskArray === tasks.toDo) {
+        htmlList = document.getElementById('toDoList')
+        startIndex = tasks.toDo.length - 1 ;
+    } else if (taskArray === tasks.notToDo) {
+        htmlList = document.getElementById('notToDoList')
+        startIndex = tasks.notToDo.length - 1 ;
+    } else {
+        console.error('Unknown task type:', taskArray);
+    };
+
+    for (let i = startIndex; i < taskArray.length; i++) {
+        let listItem = document.createElement('li');
+        listItem.innerHTML = `<button class="deleteButton" onclick='removeTaskFromList(this)'>X</button>
+            <p class="listItem">${taskArray[i]}</p>`;
+        htmlList.appendChild(listItem);
+    };
+    resetInputField()
+}
+
+const resetInputField = () => {
+    inputField.value = '';
+    // inputField.placeholder = 'Enter another task here...';
 }
 
 const removeTaskFromList = (e) => {
     e.parentNode.remove();
 }
 
-const addTaskToList = (button) => {
-    const newToDo = inputField.value;
-    if (button === 'toDoButton') {
-        createListItem(newToDo, 'toDoButton');
-        resetInputField(newToDo, 'toDoButton');
-    } else if (button === 'notToDoButton') {
-        createListItem(newToDo, 'notToDoButton');
-        resetInputField(newToDo, 'notToDoButton');
-    }
-}
 
-const createListItem = (newTask, button) => {
-    // Frank: je controleert drie keer of de newTask leeg is
-    // het kan ook 1 keer gechecked worden
-    if (newTask !== '' && button === 'toDoButton') {
-        let listItem = document.createElement('li');
-        listItem.innerHTML = `<button class="deleteButton" onclick='removeTaskFromList(this)'>X</button>
-        <p class="listItem">${newTask}</p>`;
-        document.getElementById('toDoList').appendChild(listItem);
-    } else if (newTask !== '' && button === 'notToDoButton') {
-        let listItem = document.createElement('li');
-        listItem.innerHTML = `<button class="deleteButton" onclick='removeTaskFromList(this)'>X</button>
-        <p class="listItem">${newTask}</p>`;
-        document.getElementById('notToDoList').appendChild(listItem);
-    }
-}
-
-
-const resetInputField = (newTask) => {
-    if (newTask !== '') {
-        inputField.value = ''
-        inputField.placeholder = 'Enter another task here...'
-    }
-}
+renderTasks(tasks.toDo);
+renderTasks(tasks.notToDo)
